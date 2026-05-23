9daac72f1d7c7272c5e39ce0b3882797e79302a2
ThitiwutM <thitiwut.ma@gmail.com>
2026-05-23 13:39:39 +0700

    Decrease confidence threshold for model predictions

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if uploaded_file is not None:
 
     results = model.predict(
         source=img,
-        conf=0.9,
+        conf=0.85,
         imgsz=1280,
         augment=True,
         verbose=False
