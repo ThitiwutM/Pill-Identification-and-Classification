72183d056a8bafbacdf9e1d8bf7cc27e6e2b1395
ThitiwutM <thitiwut.ma@gmail.com>
2026-05-23 13:29:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if uploaded_file is not None:
     results = model.predict(
         source=img,
         conf=0.90,
-        imgsz=1280,
+        imgsz=640,
         augment=True,
         verbose=False
     )
